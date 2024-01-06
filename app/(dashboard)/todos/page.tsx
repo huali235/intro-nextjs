4c887251bbbb64183313730e20b41a5079d0623b
Hussain Ali <huali235@gmail.com>
2024-01-06 12:02:31 -0500

    minor changes

--- app/(dashboard)/todos/page.tsx
+++ app/(dashboard)/todos/page.tsx
@@ -2,6 +2,7 @@ import TodoList from '@/app/components/TodoList'
 import db from '@/utils/db'
 
 async function getData() {
+  // await new Promise((resolve, reject) => setTimeout(reject, 2000))
   const todos = await db.todo.findMany()
   return todos
 }
---
+++ app/api/todo/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server'
+import db from '@/utils/db'
+
+export async function GET(request: Request) {
+  const todos = await db.todo.findMany()
+  return NextResponse.json({ data: todos })
+}
+
+export async function POST(request: Request) {
+  const data = await request.json()
+  const todo = await db.todo.create({ data })
+  return NextResponse.json({ message: todo })
+}

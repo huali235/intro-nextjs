31c15e93376148e8ad18585d492401f7f0c3341d
Hussain Ali <huali235@gmail.com>
2024-01-06 12:01:58 -0500

    handle complete task

--- app/components/Todo.tsx
+++ app/components/Todo.tsx
@@ -1,6 +1,24 @@
+'use client'
+
+import { completeTodo } from '@/utils/actions'
+import { useTransition } from 'react'
+
 function Todo({ todo }) {
+  const [isPending, startTransition] = useTransition()
+
+  function handleClick() {
+    startTransition(() => {
+      completeTodo(todo.id)
+    })
+  }
+
   return (
-    <div>
+    <div
+      className={`border border-black/20 cursor-pointer ${
+        todo.isCompleted ? 'line-through text-gray-900' : ''
+      }`}
+      onClick={handleClick}
+    >
       <p>{todo.content}</p>
     </div>
   )
--- utils/actions.ts
+++ utils/actions.ts
@@ -2,6 +2,15 @@
 import { revalidatePath } from 'next/cache'
 import db from './db'
 
+export async function completeTodo(id: string) {
+  await db.todo.update({
+    where: { id },
+    data: { isCompleted: true },
+  })
+
+  revalidatePath('/todos')
+}
+
 export async function newTodo(formData) {
   const todo = await db.todo.create({
     data: { content: formData.get('content') },
